1ad089421d779c22bb87501c10185798211ae94c
Sushil Subedi <159403641+SUSHILIZ@users.noreply.github.com>
2024-03-28 06:33:57 +0000

    modify

--- practice.js
+++
@@ -1,4 +0,0 @@
-let name="Sushil Subedi"
-let FirstName=name.slice(0, name.indexOf(" "));
-let LastName=name.slice(name.indexOf(" ")+1);
-console.log(`Firstname is ${FirstName} and Lastname is ${LastName}`);
